e85900940b8d18f47aaa2c7b889d3ecab153b8f3
JenerikMind <darkheart_wolf@yahoo.com>
2024-01-13 14:16:16 -0500

    Theme and styling - added spacing to layout - added elevation to text cards

--- app/src/main/java/com/example/jokeaday/ui/jokePresentation/JokePresentation.kt
+++ app/src/main/java/com/example/jokeaday/ui/jokePresentation/JokePresentation.kt
@@ -1,9 +1,14 @@
 package com.example.jokeaday.ui.jokePresentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,8 +29,10 @@ fun JokeDisplay(
         val safeSetup = setup ?: "Here's a setup..."
         val safePunchline = punchline ?: "Here's a punchline... Pow! Right in the kisser."
 
-        Text(text = "Setup: $safeSetup")
-        Text(text = "Punchline: $safePunchline")
+        TextBox(text = "Setup: $safeSetup")
+        Spacer(modifier = Modifier.height(8.dp))
+        TextBox(text = "Punchline: $safePunchline")
+        Spacer(modifier = Modifier.height(8.dp))
         Button(onClick = onClick) {
             Text(stringResource(id = R.string.another_one))
         }
@@ -34,6 +41,20 @@ fun JokeDisplay(
 
 @Composable
 @Preview
-fun TextBox(){
-
+fun TextBox(text: String="") {
+    Card(modifier = Modifier
+        .padding(horizontal = 16.dp)
+        .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        )
+    ) {
+        Text(
+            modifier = Modifier.padding(
+                vertical = 16.dp,
+                horizontal = 8.dp
+            ),
+            text = text
+        )
+    }
 }
