2c9e6cd40fb84473b77a3f2fea3394cb4c0f5a3f
JenerikMind <darkheart_wolf@yahoo.com>
2024-01-13 17:05:22 -0500

    Theme and styling - added custom font from google fonts - changed base font family and weight

--- app/src/main/java/com/example/jokeaday/ui/theme/Type.kt
+++ app/src/main/java/com/example/jokeaday/ui/theme/Type.kt
@@ -2,15 +2,22 @@ package com.example.jokeaday.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.jokeaday.R
+
+val customFontFamily = FontFamily(
+    Font(R.font.biorhymeregular, FontWeight.Normal),
+    Font(R.font.biorhymebold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = customFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.8.sp,
