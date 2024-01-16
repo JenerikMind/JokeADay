a6365f5d7de3578c73e5ac77fbc7bc1334a580e4
JenerikMind <darkheart_wolf@yahoo.com>
2024-01-16 16:34:52 -0500

    added styling to bottom nav
    - added icon styling to bottom nav
    - changed to material 3 usage of text and icons

--- app/src/main/java/com/example/jokeaday/ui/reusableComposables/CustomScaffold.kt
+++ app/src/main/java/com/example/jokeaday/ui/reusableComposables/CustomScaffold.kt
@@ -1,21 +1,28 @@
 package com.example.jokeaday.ui.reusableComposables
 
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -25,6 +32,7 @@ import com.example.jokeaday.R
 import com.example.jokeaday.ui.screens.Screen
 import com.example.jokeaday.ui.theme.DarkGreen
 import com.example.jokeaday.ui.theme.Purple40
+import com.example.jokeaday.ui.theme.heightMedium
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,11 +41,6 @@ fun CustomScaffold(
     saveJoke: () -> Unit,
     content: @Composable (padding: PaddingValues) -> Unit
 ) {
-    val currentDestinationIsJoke = navController.currentDestination?.route == "JokePresentation"
-    val bottomButtonColors =
-        if (currentDestinationIsJoke) listOf(Color.White, Purple40) else listOf(
-            Purple40, Color.White
-        )
     val bottomNavItems = listOf(
         Screen.Joke,
         Screen.Favorites
@@ -55,13 +58,17 @@ fun CustomScaffold(
             )
         },
         bottomBar = {
-            BottomNavigation {
+            NavigationBar(
+                modifier = Modifier.height(heightMedium),
+                containerColor = Purple40
+            ) {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentDestination = navBackStackEntry?.destination
+
                 bottomNavItems.forEach { screen ->
-                    BottomNavigationItem(
-                        icon = { /*TODO*/ },
-                        label = { Text(stringResource(id = screen.resId)) },
+                    NavigationBarItem(
+                        icon = { Icon(painterResource(id = screen.drawId), contentDescription = null) },
+                        label = {},
                         selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
                         onClick = {
                             navController.navigate(screen.route) {
@@ -71,36 +78,10 @@ fun CustomScaffold(
                                 launchSingleTop = true
                                 restoreState = true
                             }
-                        }
+                        },
                     )
                 }
             }
-
-
-//            BottomAppBar(
-//                contentColor = Purple40,
-//                containerColor = Purple40,
-//                modifier = Modifier
-//                    .height(heightMedium)
-//            ) {
-//                CustomTextButton(
-//                    onClick = { navController.navigate("JokePresentation") },
-//                    modifier = Modifier
-//                        .weight(1f)
-//                        .background(bottomButtonColors[0]),
-//                    resId = R.string.joke,
-//                    textColor = bottomButtonColors[1]
-//                )
-//                CustomTextButton(
-//                    onClick = { navController.navigate("FavoritesPresentation") },
-//                    modifier = Modifier
-//                        .weight(1f)
-//                        .background(bottomButtonColors[1])
-//                        .fillMaxSize(),
-//                    resId = R.string.favorites,
-//                    textColor = bottomButtonColors[0]
-//                )
-//            }
         },
         floatingActionButton = {
             FloatingActionButton(
